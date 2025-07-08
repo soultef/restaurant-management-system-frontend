98745d3314ae9697dbc7e1ebf0f00ac5d11cd906
Solomon Demisse <soul_tefera@yahoo.com>
2025-07-08 14:27:46 -0500

    update nav bar and css to make it responsive

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,31 +1,52 @@
-import React, {useState} from 'react'
-import { FaSearch} from 'react-icons/fa';
-import { FaUser,FaCartShopping } from 'react-icons/fa6';
-import './CSS/NavBar.css'
-
-
+import React, { useState } from 'react';
+import { FaSearch, FaBars, FaTimes } from 'react-icons/fa';
+import { FaUser, FaCartShopping } from 'react-icons/fa6';
+import './CSS/NavBar.css';
 
 function NavBar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
 
   return (
-    <nav className='nav-bar'>
-      <ul className='menu-nav-link'>
+    <nav className="nav-bar">
+      
+      <ul className="menu-nav-link">
+        <li onClick={toggleSidebar}>
+          <FaBars />
+        </li>
         <li><a href="#">Logo</a></li>
         <li><a href="#">MENUE</a></li>
         <li><a href="#">SPECIALS</a></li>
         <li><a href="#">RESERVE</a></li>
         <li><a href="#">JOBS</a></li>
-        <li><a href="#">LOCATION</a></li> 
+        <li><a href="#">LOCATION</a></li>
       </ul>
 
-      <ul className='cart-nav-link'>
-        <li><a href="#"><FaSearch/></a></li>
-        <li><a href="#"><FaCartShopping/></a></li>
-        <li><a href="#"><FaUser/></a></li>
-        <li className='order-link'><a href="#">ORDER NOW</a></li>
+      
+      <ul className={`menu-side-bar ${sidebarOpen ? 'show' : ''}`}>
+        <li onClick={toggleSidebar}>
+          <FaTimes />
+        </li>
+        <li><a href="#">Logo</a></li>
+        <li><a href="#">MENUE</a></li>
+        <li><a href="#">SPECIALS</a></li>
+        <li><a href="#">RESERVE</a></li>
+        <li><a href="#">JOBS</a></li>
+        <li><a href="#">LOCATION</a></li>
+      </ul>
+
+      
+      <ul className="cart-nav-link">
+        <li><a href="#"><FaSearch /></a></li>
+        <li><a href="#"><FaCartShopping /></a></li>
+        <li><a href="#"><FaUser /></a></li>
+        <li className="order-link"><a href="#">ORDER NOW</a></li>
       </ul>
     </nav>
-  )
+  );
 }
 
 export default NavBar;
